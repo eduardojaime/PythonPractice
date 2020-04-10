1b19c6e5a04ba29c9505683c1762583b7fc82246
Eduardo Jaime <eduardojaime86@gmail.com>
2020-04-09 20:02:46 -0400

    Error Handling examples

--- error_handling.py
+++ error_handling.py
@@ -25,6 +25,31 @@ while try_again == True:
             try_again = False
     except ValueError:
         print('Invalid number!')
+    except KeyboardInterrupt:
+        exit_option = input('Are you sure you want to Exit? Y or N')
+        if exit_option.upper() == 'Y':
+            try_again = False
+            print('Ending program...')
+print('\n')
+
+try_again = True
+while try_again == True:
+    try:
+        input_text = input('Enter a number from 1 to infinity, to see its Times Table:')
+        input_int = int(input_text)
+        if input_int <= 0:
+            print('Negative numbers are not allowed')
+        else:
+            times_val = 1
+            while times_val <= 10:
+                print(input_int, 'times', times_val, 'equals', input_int * times_val)
+                times_val = times_val + 1
+            exit_option = input('Press any key to try again, or press E to exit.')
+            if exit_option.upper() == 'E':
+                try_again = False
+            
+    except ValueError:
+        print('Invalid number! Enter a numeric value')
         
 print('Program ended')
 print('\n')
